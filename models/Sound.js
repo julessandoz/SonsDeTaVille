257eab725bb76ce9dfe1413ab75772244eb0be19
Jules Sandoz <jules.sandoz@heig-vd.ch>
2022-10-25 17:33:58 +0200

    updated sound schema to correctly implement geolocation to the location property

--- models/Sound.js
+++ models/Sound.js
@@ -6,10 +6,45 @@ const Category = mongoose.model("Category", categorySchema);
 // Define a schema
 const soundSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  location: { type: String, required: true },
+  location: {
+    type: { type: String, required: true, enum: ["Point"] },
+    coordinates: {
+      type: [Number],
+      required: true,
+      validate: {
+        validator: validateGeoJsonCoordinates,
+        message:
+          "{VALUE} is not a valid longitude/latitude(/altitude) coordinates array",
+      },
+    },
+    required: true,
+  },
   sound: { type: Buffer, required: true },
   category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
   date: { type: Date, default: Date.now }, // Default value is now
 });
+
+// Create a geospatial index on the location property.
+geolocatedSchema.index({ location: "2dsphere" });
+
+// Validate a GeoJSON coordinates array (longitude, latitude and optional altitude).
+function validateGeoJsonCoordinates(value) {
+  return (
+    Array.isArray(value) &&
+    value.length >= 2 &&
+    value.length <= 3 &&
+    isLongitude(value[0]) &&
+    isLatitude(value[1])
+  );
+}
+
+function isLatitude(value) {
+  return value >= -90 && value <= 90;
+}
+
+function isLongitude(value) {
+  return value >= -180 && value <= 180;
+}
+
 // Create the model from the schema and export it
 export default mongoose.model("Sound", soundSchema);
