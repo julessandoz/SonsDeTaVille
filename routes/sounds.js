bd1e8c7d88a7d1201176cfc35cee1659c44e9e07
Jules Sandoz <jules.sandoz@heig-vd.ch>
2022-11-09 08:38:51 +0100

    started working on the sound router

--- routes/sounds.js
+++ routes/sounds.js
@@ -1,10 +1,36 @@
 import express from "express";
 import Sound from "../models/Sound.js";
+import {authenticate} from "./auth.js";
 
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
-    res.send("Got a response from the sound route");
-  });
-  
+  const { lat, lng, radius } = req.query.location;
+  const category = req.query.category;
+  const username = req.query.username;
+  const date = req.query.date;
+  const limit = req.query.limit;
+  // get sounds with the quey parameters that are not null
+  Sound.find({
+    location: {
+      $near: {
+        $geometry: {
+          type: "Point",
+          coordinates: [lng, lat],
+        },
+        $maxDistance: radius,
+      },
+    },
+    category: category,
+    user: username,
+    date: date,
+  })
+    .limit(limit)
+    .then((sounds) => {
+      res.json(sounds);
+    })
+    .catch(next);
+});
+
+
   export default router;
