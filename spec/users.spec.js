dd0b64653e4bad0ab846df10ca2d7ba389a1593c
Stéphane Sordet <stsordet@gmail.com>
2022-11-13 12:46:35 +0100

    user tests implemented

--- spec/users.spec.js
+++ spec/users.spec.js
@@ -1,12 +1,12 @@
 import supertest from "supertest"
-import mongoose from "mongoose"
+import mongoose, { trusted } from "mongoose"
 import app from "../app.js"
 import { cleanUpDatabase, generateValidJwt } from "./utils.js"
 import User from "../models/User.js"
 
 beforeEach(cleanUpDatabase); 
 
-
+// CREATE USER TEST
 describe('POST /users', function() {
     it('should create a user', async function(){
         const res = await supertest(app)
@@ -22,16 +22,16 @@ describe('POST /users', function() {
   }); 
 
 
-
+//LOGIN USER TEST
   describe('POST /auth/login', function(){
     let Jules;
     beforeEach(async function() {
-      // Create 2 users before retrieving the list.
-      [ Jules ] = await Promise.all([
+        [Jules] = await Promise.all([
         User.create({ 
             username: 'Jules',
             email: 'jules@gmail.com',
-            clearPassword: 'Test1234'
+            clearPassword: 'Test1234',
+            admin: true
         })
       ]);
     }); 
@@ -48,13 +48,13 @@ describe('POST /users', function() {
   });
 
 
-
+//GET USER LIST TEST 
   describe('GET /users', function() {
     let Jules;
     let Stephane;
     beforeEach(async function() {
       // Create 2 users before retrieving the list.
-      [ Jules, Stephane ] = await Promise.all([
+        [Jules, Stephane]  = await Promise.all([
         User.create({ 
             username: 'Jules',
             email: 'jules@gmail.com',
@@ -74,9 +74,100 @@ describe('POST /users', function() {
           .set('Authorization', `Bearer ${token}`)
           .expect(200)
           .expect('Content-Type', /json/)
+          expect(res.body).toHaveLength(2);
+          expect(res.body[0]).toBeObject();
+          expect(res.body[0]._id).toEqual(Jules.id)
+          expect(res.body[0].username).toEqual(Jules.username)
+          expect(res.body[1]).toBeObject();
+          expect(res.body[1].username).toEqual(Stephane.username)
+        
         
     });
   });
+
+
+  //GET USER BY USERNAME TEST
+  describe('GET /users/:username', function() {
+    let Jules;
+    beforeEach(async function() {
+        [Jules] = await Promise.all([
+        User.create({ 
+            username: 'Jules',
+            email: 'jules@gmail.com',
+            clearPassword: 'Test1234'
+        })
+      ]);
+    }); 
+    test('should return a user by name', async function() {
+        const token = await generateValidJwt(Jules);
+        const res = await supertest(app)
+          .get(`/users/${Jules.username}`)
+          .set('Authorization', `Bearer ${token}`)
+          .expect(200)
+          .expect('Content-Type', /json/)
+          expect(res.body).toBeObject();
+          expect(res.body._id).toEqual(Jules.id)
+          expect(res.body.username).toEqual(Jules.username)
+    });
+  });
+
+
+  
+  describe('DELETE /users/:username', function() {
+    let Jules;
+    let Stephane;
+    beforeEach(async function() { 
+        [Jules, Stephane] = await Promise.all([
+        User.create({ 
+            username: 'Jules',
+            email: 'jules@gmail.com',
+            clearPassword: 'Test1234',
+            admin: true
+        }),
+        User.create({ 
+          username: 'Stephane',
+          email: 'stephane@gmail.com',
+          clearPassword: 'Test1234',
+      })
+      ]);
+    }); 
+    test("should delete the user's account", async function() {
+        const token = await generateValidJwt(Stephane);
+        const res = await supertest(app)
+          .delete(`/users/${Stephane.username}`)
+          .set('Authorization', `Bearer ${token}`)
+          .expect(200)
+    });
+  });
+
+
+  describe('PATCH /users/:username', function() {
+    let Jules;
+    beforeEach(async function() { 
+        [Jules] = await Promise.all([
+        User.create({ 
+            username: 'Jules',
+            email: 'jules@yahoo.com',
+            clearPassword: 'Test1234',
+        })
+      ]);
+    }); 
+    test("should modify the user's password and email", async function() {
+        const token = await generateValidJwt(Jules);
+        const res = await supertest(app)
+          .patch(`/users/${Jules.username}`)
+          .set('Authorization', `Bearer ${token}`)
+          .send({
+            email: "jules@gmail.com",
+            password: "Test1234"
+          })
+          .expect(200)
+          .expect('Content-Type', /json/)
+          expect(res.body).toBeObject();
+          expect(res.body.email).not.toEqual(Jules.email)
+    });
+  });
+
 
   afterAll(async () => {
     await mongoose.disconnect();
