a55a1e50b00d081b00c7a27085873977eaa95cae
Jules Sandoz <jules.sandoz@heig-vd.ch>
2022-12-16 11:51:50 +0100

    added cors middleware

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ import commentsRouter from "./routes/comments.js";
 import * as config from "./config.js";
 import path from "path";
 import { fileURLToPath } from 'url';
+import cors from 'cors';
 
 import mongoose from 'mongoose';
 mongoose.Promise = Promise;
@@ -21,7 +22,7 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
+app.use(cors())
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
